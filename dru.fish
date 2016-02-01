e29d126574e028d3a3ea4dfcf8e0cee9828b1aae
Mathias Novas <mathias@07.no>
2016-02-01 11:23:34 +0100

    Update 'dru' with emoticons

--- dru.fish
+++ dru.fish
@@ -1,11 +1,19 @@
 function dru --argument site
+	if test $site
+        # echo "$site ＼(￣▽￣;)／"
+    else
+        echo "[o_Ô] Where are you going?!"
+        return 0
+    end
+
 	set drupal_version (ls -t /var/www/drupal/sites | head -1)
     set site_path /var/www/drupal/sites/$drupal_version/$site
 
     if test -e $site_path
 	    cd $site_path
+        echo "[づ￣ ³￣]づ $site"
     else
-        set_color red; and echo 'Site does not exist.' 
+        set_color red; and echo "[^._.^]ﾉ $site does not exist."
         set_color normal; and echo "Run: drupal addsite -s $site -v $drupal_version" 
     end
 end
