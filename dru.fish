8562747f86e9d6ae7176180fa90afc7d3dc738fa
Mathias Novas <mathias@07.no>
2016-02-01 11:05:06 +0100

    Update 'dru' function
    Checks if the site exists and suggests to run drupal
    command for adding the site it doesn't.

--- dru.fish
+++ dru.fish
@@ -1,5 +1,11 @@
 function dru --argument site
 	set drupal_version (ls -t /var/www/drupal/sites | head -1)
+    set site_path /var/www/drupal/sites/$drupal_version/$site
 
-	cd /var/www/drupal/sites/$drupal_version/$site
+    if test -e $site_path
+	    cd $site_path
+    else
+        set_color red; and echo 'Site does not exist.' 
+        set_color normal; and echo "Run: drupal addsite -s $site -v $drupal_version" 
+    end
 end
