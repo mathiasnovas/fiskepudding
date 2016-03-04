d9be770b5c5ecd576a717c33036afd9372fb8247
Mathias Novas <mathias@07.no>
2016-03-04 10:33:24 +0100

    Remove user specific functions from git

--- bower.fish
+++
@@ -1,3 +0,0 @@
-function bower --argument task param param2
-	command bower $task $param $param2 --allow-root
-end
--- mountvm.fish
+++
@@ -1,3 +0,0 @@
-function mountvm
-	mkdir -p /Volumes/vm; and sshfs vm:/var/www /Volumes/vm
-end
